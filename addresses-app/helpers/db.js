f493b15c85abcef8dbd57fd2f65047ad86bba5ae
Augusto <augustoslatge@gmail.com>
2019-11-06 14:03:14 -0500

    before downgrading the expo SDK version from 35 to 34

---
+++ addresses-app/helpers/db.js
@@ -0,0 +1,23 @@
+import { SQLite } from "expo-sqlite";
+
+console.log('test');
+
+const db = SQLite.openDatabase("places.db");
+
+export const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS places (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, imageUri TEXT NOT NULL, address TEXT NOT NULL, lat REAL NOT NULL, lng REAL NOT NULL);",
+        [],
+        () => {
+          resolve();
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
