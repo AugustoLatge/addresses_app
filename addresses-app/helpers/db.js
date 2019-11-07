913739c24d7260e6e0ca4dc692dd76f1824991aa
Augusto <augustoslatge@gmail.com>
2019-11-07 16:08:52 -0500

    Complete Address App - React Native Tutorial - Section 12

--- addresses-app/helpers/db.js
+++ addresses-app/helpers/db.js
@@ -1,6 +1,4 @@
-import { SQLite } from "expo-sqlite";
-
-console.log('test');
+import * as SQLite from "expo-sqlite";
 
 const db = SQLite.openDatabase("places.db");
 
@@ -12,6 +10,42 @@ export const init = () => {
         [],
         () => {
           resolve();
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
+
+export const insertPlace = (title, imageUri, address, lat, lng) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);`,
+        [title, imageUri, address, lat, lng],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  });
+  return promise;
+};
+
+export const fetchPlaces = () => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        "SELECT * FROM places",
+        [],
+        (_, result) => {
+          resolve(result);
         },
         (_, err) => {
           reject(err);
